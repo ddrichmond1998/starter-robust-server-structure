c11c87da5874f746b8f2e844d01ad9ef4c7d1409
ddrichmond1998 <drichmondhealthexec@gmail.com>
2022-02-05 00:00:37 -0600

    j

--- src/app.js
+++ src/app.js
@@ -1,72 +1,24 @@
 const express = require("express");
 const app = express();
 
-const flips= require('./data/flips-data');//reads, executes, and returns the data from the flips-data.js file
-const counts= require('./data/counts-data');//reads, executes, and returns the data from the counts-data.js file
-// TODO: Follow instructions in the checkpoint to implement ths API.
-//api endpoint for new records. 
-app.use(express.json());//allows to parse request body as json. 
+const countsRouter = require("./counts/counts.router");
+const flipsRouter = require("./flips/flips.router");
 
-app.use('/counts/:countId', (req, res, next) => {
-  const {countId} = req.params;
-  const foundCount = counts[countId];
-  if (foundCount === undefined) {//counts may be 0, a falsy value, you must check if the value is undefined
-    next(`Count id not found: ${countId}`);
-  } else {
-    res.json({ data: foundCount });//return the count as json
-  }
-});
+app.use(express.json());
 
-app.use('/counts', (req, res) => {
-  res.json({ data: counts });
-});
-
-
-
-
-app.use('/flips/:flipId', (req, res, next) => {//defines a handler for the /flips/:flipId path.
-  const { flipId } = req.params;//defines flipId by destructuring the req.params object.
-  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
-  if(foundFlip) {
-    res.json({ data: foundFlip });
-  } else {
-    next(`Flip id not found: ${flipId}`);
-  }
-});
-//changing to app.get allows to get the data from the flips-data.js file. Handler will only be called if a get request
-app.get('/flips', (req, res) => {//defines a handler for the /flips path.
-  res.json({ data: flips });
-});
-//handler for post requests to /flips comes after the .get
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
-let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
-
-app.post("/flips", (req, res, next) => {
-  const { data: { result } = {} } = req.body;//standard destructuring syntax. 
-  if (result) {//added this if statement to check if the result is truthy.
-  const newFlip = {
-    id: ++lastFlipId, // Increment last ID, then assign as the current ID
-    result,
-  };
-  flips.push(newFlip);
-  counts[result] = counts[result] + 1; // Increment the counts (heads, tails or edge)
- // res.json({ data: newFlip }); sends the new response to the client
- res.status(201).json({ data: newFlip });//sends the new response to the client with a status code of 201 when a successful post request is made.
-} else {//if the result is falsy, send a 400 error.
-   res.sendStatus(400);
-  }
-});
+app.use("/counts", countsRouter);
+app.use("/flips", flipsRouter);
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  next({ status: 404, message: `Not found: ${request.originalUrl}` });
 });
 
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
 });
 
 module.exports = app;
